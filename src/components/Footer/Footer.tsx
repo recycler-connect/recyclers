f1d443df68d4fe463ba08fedcd132e1ac09154f6
Emily Sellers <emilymelindasellers@gmail.com>
2023-03-01 16:25:52 -0800

    update alignment and spacing of components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  return <div className="footer">developed 2023</div>;
+  return <div className="footer">Developed by Hana Acciaioli & Emily Sellers 2023</div>;
 }
