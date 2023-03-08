a248c2055fe42b976ef8aba5ef694bef8c255d5a
Emily Sellers <emilymelindasellers@gmail.com>
2023-03-08 11:06:25 -0800

    convert App.js and Main.js to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-// import { Redirect, Route, Switch } from 'react-router-dom';
+import React from 'react';
 import './App.css';
-// import Auth from './components/Auth/Auth';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main.js';
+// import Auth from './components/Auth/Auth';
+// import { Redirect, Route, Switch } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      // <Header />
-      // <Main />
-      // <Footer />
-      //{' '}
+      <Header />
+      <Main />
+      <Footer />
       {/* <Switch>
     //     <Route path="/auth/:type" component={Auth} />
     //     <Route path="*">
