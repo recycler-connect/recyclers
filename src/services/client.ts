0e305d8a70e82ce599b54380ee56ceff0b9fdca9
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-03-09 16:22:46 -0800

    updated to supabase version 2.0 and implemented createClient with types

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,15 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
-
 import type { Database } from '../../db_types';
+//converting URL & KEY to string so that it can be used in createClient
+const configSupabaseURLValue: string = process.env.REACT_APP_SUPABASE_URL as string;
+const configSupabaseKeyValue: string = process.env.REACT_APP_SUPABASE_KEY as string;
 
-export const client = createClient<Database>(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_KEY
-);
+export const client = createClient<Database>(configSupabaseURLValue, configSupabaseKeyValue);
 
-export function checkError({ data, error }) {
-  if (error) {
-    throw error;
-  }
-  return data;
-}
+// export function checkError({ data, error }) {
+//   if (error) {
+//     throw error;
+//   }
+//   return data;
+// }
--- src/services/recyclers.ts
+++
@@ -1,6 +0,0 @@
-import { client, checkError } from './client';
-
-export async funtion getRecyclersBasedOnMaterial() {
-    const resp = await client.from('materials').select('*');
-    return checkError(resp);
-};
