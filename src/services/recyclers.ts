7c85a352bfa4fbfee828c5d346dd3ce1703fa78e
Emily Sellers <emilymelindasellers@gmail.com>
2023-03-13 15:23:43 -0700

    add comment to add getRecyclers error handler

--- src/services/recyclers.ts
+++ src/services/recyclers.ts
@@ -2,5 +2,6 @@ import { client } from './client';
 
 export async function getRecyclers() {
   const recyclerResp = await client.from('recyclers').select('*');
+  //add error handler here
   return recyclerResp.data;
 }
