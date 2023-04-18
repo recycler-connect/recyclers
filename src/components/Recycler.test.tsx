627dcf60fc8cde3f50beae3f74ab310e56638c70
Molly Jean Bennett <molly.bennett@equipmentshare.com>
2023-04-18 16:41:08 -0700

    Add initial test

---
+++ src/components/Recycler.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Recycler from './Recycler';
+import { getMockRecycler } from 'src/mocks/mockRecycler';
+
+describe('Recycler component', () => {
+
+  it('should render the component and show recycler data', () => {
+    const mockRecycler = getMockRecycler();
+
+    const { getByText } = render(<Recycler {...mockRecycler} />);
+    expect(getByText(mockRecycler.company)).toBeInTheDocument();
+  });
+});
