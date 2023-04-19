83fb0e3cca7a2b064c0a2eaa89c90e7bbae0ef14
Molly Jean Bennett <molly.bennett@equipmentshare.com>
2023-04-18 17:06:37 -0700

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
