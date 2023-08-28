fde647eb78c60f21f6beaad7f17f8c981f5f639b
Emily Sellers <emilymelindasellers@gmail.com>
2023-08-28 16:29:11 -0700

    Update fonts and alignment

--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { RecyclerContext } from 'src/context/RecyclerMainPage';
 // import { recyclers } from 'src/data';
 import Recycler from './Recycler';
+import './Main/Main.css';
 
 export default function ResultsList() {
   const { recyclerResults } = useContext(RecyclerContext);
 
   return (
-    <div>
-      <h3>You have {recyclerResults.length} matching recyclers. </h3>
+    <div className="results-list">
+      <h3>You have {recyclerResults.length} matching recyclers: </h3>
       {recyclerResults.length !== 0 &&
         // tsx error: Property 'data' does not exist on type 'RecyclerResultType[]'.ts(2339)
         recyclerResults.map((recycler) => <Recycler key={recycler.id} {...recycler} />)}
