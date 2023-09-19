48dbe6dce985c08ec21ef5ff8a4040046cbb67d2
Emily Sellers <emilymelindasellers@gmail.com>
2023-09-19 13:20:15 -0700

    Update recycler card content and box-shadow

--- src/components/Recycler.tsx
+++ src/components/Recycler.tsx
@@ -3,5 +3,11 @@ import { RecyclerResultType } from 'src/context/RecyclerMainPage';
 import './Recycler.css';
 
 export default function Recycler(props: RecyclerResultType) {
-  return <div className="recycler">{props.company}</div>;
+  return (
+    <div className="recycler">
+      <h3>{props.company}</h3>
+      <p>{props.location}</p>
+      <p>URL placeholder</p>
+    </div>
+  );
 }
