05db47715e69a3ccd6cbe771708bee582824ce68
Emily Sellers <emilymelindasellers@gmail.com>
2023-09-22 10:51:32 -0700

    Update footer color styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  return <div className="footer">Developed by Hana Acciaioli & Emily Sellers 2023</div>;
+  const myStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/redcloth.jpeg'})`,
+    height: '50%',
+    // marginTop: '-30px',
+    fontSize: '40px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
+
+  return <div className="footer">© Hana Acciaioli & Emily Sellers 2023</div>;
 }
