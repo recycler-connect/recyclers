c960f86cb3bcf17c74e9dab5f6ce44005a69688e
Emily Sellers <emilymelindasellers@gmail.com>
2023-09-22 11:05:05 -0700

    Remove unused code to fix Netlify fail

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,14 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  const myStyle = {
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/redcloth.jpeg'})`,
-    height: '50%',
-    // marginTop: '-30px',
-    fontSize: '40px',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-  };
-
   return <div className="footer">© Hana Acciaioli & Emily Sellers 2023</div>;
 }
