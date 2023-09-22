6b571b2b1e5b07775fc75c3fae30957053d928dd
Emily Sellers <emilymelindasellers@gmail.com>
2023-09-21 22:04:54 -0700

    Add header background image and placeholder logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,9 +14,26 @@ export default function Header() {
   //     console.error(error.message);
   //   }
   // };
+  const myStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/redcloth.jpeg'})`,
+    height: '50%',
+    // marginTop: '-30px',
+    fontSize: '40px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
+
   return (
-    <header>
-      <h2>Recycler Connect</h2>
+    <header style={myStyle}>
+      <img
+        src="/shirt.png"
+        // src={
+        //   'https://png.pngtree.com/png-clipart/20220910/original/pngtree-semicircle-frame-simple-png-image_8531222.png'
+        // }
+        width="40"
+        // height="50"
+      />
+      <p className="header-title">recycler connect</p>
       {/* {user && (
         <>
           <div className="welcome">Welcome {user.email}</div>{' '}
